a5b0449b03cc2c9d7402990fa93e70acb8715c71
Danil <daniil-vn@mail.ru>
2025-01-11 01:28:03 +0500

    Написана программа, которая позволяет понять, какие 10 слов чаще всего встречаются в тексте.

--- TaskThirteen.Six.Two/Program.cs
+++ TaskThirteen.Six.Two/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace TaskThirteen.Six.Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Чтение текста из файла
+            string text = File.ReadAllText("C:\\Users\\user\\Desktop\\input.txt");
+
+            // Удаление знаков пунктуации
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            // Приведение текста к нижнему регистру и разделение на слова
+            var words = noPunctuationText.ToLower().Split(new char[] { ' ', '\n', '\r' });
+
+            // Подсчет частоты слов
+            var wordCounts = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (wordCounts.ContainsKey(word))
+                {
+                    wordCounts[word]++;
+                }
+                else
+                {
+                    wordCounts[word] = 1;
+                }
+            }
+
+            // Получение 10 самых частых слов
+            var topWords = wordCounts.OrderByDescending(kvp => kvp.Value).Take(10);
+
+            //Вывод результата
+            foreach (var kvp in topWords)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+        }
+    }
+}
